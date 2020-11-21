e4614067db90f801ddea34bd92e8ac7da6918473
Henry Jiang <henryjiang97@gmail.com>
2020-11-21 14:06:49 -0800

    Updated Main.js

--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Axios from 'axios';
+import { v4 as uuidv4 } from 'uuid';
 
 export default class Main extends Component {
     constructor(props) {
@@ -11,7 +12,6 @@ export default class Main extends Component {
         this.onLongUrlInputChange = this.onLongUrlInputChange.bind(this);
         this.onShortUrlInputChange = this.onShortUrlInputChange.bind(this);
         this.onGetButtonClick = this.onGetButtonClick.bind(this);
-        this.onPostButtonClick = this.onPostButtonClick.bind(this);
         this.getUrl = this.getUrl.bind(this);
     }
 
@@ -24,36 +24,49 @@ export default class Main extends Component {
     }
 
     onGetButtonClick() {
+        document.getElementById("url").value = "";
         this.getUrl();
     }
 
-    onPostButtonClick() {
-        this.postUrl();
-    }
-
     getUrl() {
+        const that = this;
         const long_url = this.state.long_url;
+
         Axios.get(`http://localhost:3000/api/url/${long_url}`)
             .then(function(response) {
                 console.log(response.data);
+
+                if (response.data !== "") {    // Found url in the db
+                    alert("Url exist in the database");
+                    that.setState({
+                        long_url: response.data.long_url,
+                        short_url: response.data.short_url
+                    });
+                } else {    // Not found url in the db
+                    alert("No short version for this url, created one.");
+                    let short_url = uuidv4();
+                    that.postUrl(long_url, short_url);
+                }
             })
             .catch(function(error) {
                 console.log(error);
-            })
-            .then(function() {
-                
             });
     }
 
-    postUrl() {
-        const long_url = this.state.long_url;
-        const short_url = this.state.short_url;
+    postUrl(long_url, short_url) {
+        const that = this;
+
         Axios.post('http://localhost:3000/api/url', {
             long_url: long_url,
             short_url: short_url
         })
         .then(function(response) {
             console.log(response.data);
+
+            that.setState({
+                long_url: response.data[0].long_url,
+                short_url: response.data[0].short_url
+            });
         })
         .catch(function(error) {
             console.log(error);
@@ -66,17 +79,25 @@ export default class Main extends Component {
                 <h1>URL Shortener</h1>
                 
                 <div>
-                    <label>Your url: </label>
-                    <input type="url" onChange={this.onLongUrlInputChange}></input>
+                    <label>URL: </label>
+                    <input type="url" id="url" onChange={this.onLongUrlInputChange}></input>
+                </div>
+
+                {/* <div>
+                    <label>Short url: </label>
+                    <input type="url" onChange={this.onShortUrlInputChange}></input>
+                </div> */}
+
+                <div>
+                    <button onClick={this.onGetButtonClick}>GET</button>
                 </div>
 
                 <div>
-                    <label>Short url: </label>
-                    <input type="url" onChange={this.onShortUrlInputChange}></input>
+                    <h3>Result</h3>
+                    <h4>Long URL: {this.state.long_url}</h4>
+                    <h4>Short URL: {this.state.short_url}</h4>
                 </div>
 
-                <button onClick={this.onGetButtonClick}>GET</button>
-                <button onClick={this.onPostButtonClick}>POST</button>
             </div>
         );
     }
