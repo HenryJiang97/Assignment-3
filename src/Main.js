5e03d69c89e329fcfc7b0e7ff93e5109a85145bb
wang.xin10 <wang.xin10@husky.neu.edu>
2020-12-02 16:22:51 -0800

    update validURL

--- src/Main.js
+++ src/Main.js
@@ -41,7 +41,7 @@ export default class Main extends Component {
     }
 
     validURL(str) {
-        var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+        var pattern = new RegExp('^(https?:\\/\\/)'+ // protocol
             '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|'+ // domain name
             '((\\d{1,3}\\.){3}\\d{1,3}))'+ // ip (v4) address
             '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ //port
